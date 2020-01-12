16bd52b588a04190d52aeee207889672b3471efb
laure <Laure.Gonnord@ens-lyon.fr>
2020-01-12 14:41:04 +0100

    rm

--- TP03/MiniC-type-interpret/ex-types/bad_type_bool_bool.c
+++
@@ -1,9 +0,0 @@
-#include "printlib.h"
-
-int main(){
-  println_int(true+true);
-  return 0;
-}
-// EXITCODE 2
-// EXPECTED
-// In function main: Line 4 col 14: invalid type for additive operands: boolean and boolean
--- TP03/MiniC-type-interpret/ex/test00.c
+++
@@ -1,9 +0,0 @@
-#include "printlib.h"
-
-int main(){
-  println_int(42);
-  return 0;
-}
-  
-// EXPECTED
-// 42
